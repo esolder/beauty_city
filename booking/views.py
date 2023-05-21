6112ee13b40ded6757f9698f875845a87c7647cb
Esolder <40249820+Esolder@users.noreply.github.com>
2023-05-21 18:28:10 +0300

    first commit

---
+++ booking/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.views.generic import TemplateView
+
+class AppointmentView(TemplateView):
+    template_name = 'service.html'
+
+    def get(self, request):
+        return render(request, self.template_name)
