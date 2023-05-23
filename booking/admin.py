7aebc37a29d8657b9ca91da40d55b4397a1a6796
Esolder <40249820+Esolder@users.noreply.github.com>
2023-05-23 22:55:42 +0300

    register models in admin

--- booking/admin.py
+++ booking/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Category, Service, Specialty, Employee, Appointment
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    ...
+
+
+@admin.register(Service)
+class ServiceAdmin(admin.ModelAdmin):
+    list_display = ['name', 'category', 'price']
+
+
+@admin.register(Specialty)
+class SpecialtyAdmin(admin.ModelAdmin):
+    ...
+
+
+@admin.register(Employee)
+class EmployeeAdmin(admin.ModelAdmin):
+    ...
+
+
+@admin.register(Appointment)
+class AppointmentAdmin(admin.ModelAdmin):
+    list_display = ['id', 'service', 'date', 'time', 'employee']
